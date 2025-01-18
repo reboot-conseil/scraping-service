66fab14ec486f7ca1290ece0cf58d73d3f35f762
yactouat <yacine.touati.pro@gmail.com>
2025-01-18 17:40:10 +0100

    not versioning dist anymore

--- dist/infra/ApiResponseInfra.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class ApiResponseInfra {
-    constructor(msg, data = null) {
-        this.data = data;
-        this.msg = msg;
-    }
-}
-exports.default = ApiResponseInfra;
